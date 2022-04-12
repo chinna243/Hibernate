325c9d00b4aa82a6144eafa8e35e5ddf27227fa6
kumar <saichinna243@gmail.com>
2022-04-12 15:51:46 +0530

    added this code

---
+++ demos/src/demos/practise.java
@@ -0,0 +1,14 @@
+package demos;
+
+public class practise {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("iam learning github");
+
+System.out.println("iam learning github");
+	}
+	
+
+}
