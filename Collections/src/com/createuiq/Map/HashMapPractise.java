75b45d04742faf54d06ec3dc027f94133d8b7912
kumar <saichinna243@gmail.com>
2022-04-12 12:57:21 +0530

    add collections

---
+++ Collections/src/com/createuiq/Map/HashMapPractise.java
@@ -0,0 +1,21 @@
+package com.createuiq.Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapPractise {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Map<String, Integer> map=new HashMap<>();
+		
+		map.put("FB", 1);
+		map.put("Ea", 2);
+		
+		
+	} 
+
+}
